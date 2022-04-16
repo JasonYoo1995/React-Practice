14783168cda5a8504ab0b6eeb01f32b640933085
JasonYoo1995 <jasonyoo950909@gmail.com>
2022-04-16 14:12:39 +0900

    state 추가, render() 내에서 연산

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import Subject from './components/Subject';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            subject:{title:'WEB', sub:'World Wide Web'}
+        }
+    }
     render() {
         return (
             <div className="App">
-                <Subject title={"web"} sub="world wide web"></Subject>
+                <Subject
+                    title={this.state.subject.title}
+                     sub={this.state.subject.sub}></Subject>
                 <Subject title={"different"} sub="props"></Subject>
             </div>
         )
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -2,10 +2,13 @@ import React, {Component} from "react";
 
 class Subject extends Component {
     render() {
+        let any = "render() { ... } 블록 내에서, ";
+        any += "아무 연산이나 코딩한 다음, ";
         return (
             <header>
                 <h1>{this.props.title}</h1>
                 {this.props.sub}
+                <h3>{any+ 'return ( ... ) 안에서 사용할 수 있음'}</h3>
             </header>
         )
     }
