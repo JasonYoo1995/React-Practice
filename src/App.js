4d533dbd88d44e44a969de194ee359ba34b75cca
JasonYoo1995 <jasonyoo950909@gmail.com>
2022-04-16 13:50:20 +0900

    each component is in different files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Subject from './components/Subject';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          Hello~
-        </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                <Subject title={"web"} sub="world wide web"></Subject>
+                <Subject title={"different"} sub="props"></Subject>
+            </div>
+        )
+    }
 }
 
 export default App;
